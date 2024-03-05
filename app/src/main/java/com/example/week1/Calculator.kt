f34d3cc70a54f79e5680eb61b93a3ae95d8c03ea
AndroidJeong <okbamtol@gmail.com>
2024-03-06 00:09:42 +0900

    레벨2

--- app/src/main/java/com/example/week1/Calculator.kt
+++ app/src/main/java/com/example/week1/Calculator.kt
@@ -2,6 +2,39 @@ package com.example.week1
 
 
 class Calculator {
+
+    fun calculate() {
+        var num1 = 0
+        var num2 = 0
+        var input = ""
+        var cal = ""
+
+        while (input != "1") {
+            println("계산하고 싶은 첫번째 숫자:")
+            num1 = readLine()!!.toInt()
+            println("계산하고 싶은 두번째 숫자:")
+            num2 = readLine()!!.toInt()
+            println("덧셈은 1, 뺄셈은 2, 곱셈은 3, 나눗셈은 4, 나머지 연산은 5를 입력해주세요.")
+            cal = readLine()!!
+
+            when (cal) {
+                "1" -> add(num1, num2)
+                "2" -> subtract(num1, num2)
+                "3" -> multiply(num1, num2)
+                "4" -> divide(num1, num2)
+                "5" -> remainder(num1, num2)
+                else -> println("잘못된 숫자를 입력하셨습니다.")
+            }
+
+            println("계산을 그만하시고 싶으시면 1을 입력해주세요. 계속하려면 엔터를 눌러주세요.")
+            input = readLine()!!
+
+
+        }
+
+
+    }
+
     fun add(a:Int, b:Int) {
         println(a+b)
     }
@@ -11,16 +44,19 @@ class Calculator {
     fun divide(a:Int, b:Int) {
         println(a/b)
     }
-    fun Multiply(a:Int, b:Int) {
+    fun multiply(a:Int, b:Int) {
         println(a*b)
     }
+    fun remainder(a:Int, b:Int) {
+        println(a%b)
+    }
+
 }
 
 fun main() {
 
     val example = Calculator()
-    example.add(2,2)
-    example.subtract(2,2)
-    example.divide(2,2)
-    example.Multiply(2,2)
+    example.calculate()
+
+
 }
